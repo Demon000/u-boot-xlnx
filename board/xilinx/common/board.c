4b24c470d260b7fc9ae77adbda419bf265276305
Jason Wu <jason.wu.misc@gmail.com>
2016-06-09 11:48:36 +1000

    zynq: Allow to read mac address from SPI flash OTP region
    Probe flash and use OTP read command for reading mac address stored in
    the memory. This is the easiest way how to do it.
    
    Signed-off-by: Jason Wu <jason.wu.misc@gmail.com>
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- board/xilinx/common/board.c
+++ board/xilinx/common/board.c
@@ -7,6 +7,8 @@
 #include <common.h>
 #include <dm/uclass.h>
 #include <i2c.h>
+#include <spi.h>
+#include <spi_flash.h>
 
 int zynq_board_read_rom_ethaddr(unsigned char *ethaddr)
 {
@@ -32,6 +34,33 @@ int zynq_board_read_rom_ethaddr(unsigned char *ethaddr)
 		debug("%s: I2C EEPROM MAC address read failed\n", __func__);
 	else
 		debug("%s: I2C EEPROM MAC %pM\n", __func__, ethaddr);
+#endif
+
+#if defined(CONFIG_ZYNQ_GEM_SPI_MAC_OFFSET)
+#define CMD_OTPREAD_ARRAY_FAST		0x4b
+	struct spi_flash *flash;
+	flash = spi_flash_probe(CONFIG_SF_DEFAULT_BUS,
+				CONFIG_SF_DEFAULT_CS,
+				CONFIG_SF_DEFAULT_SPEED,
+				CONFIG_SF_DEFAULT_MODE);
+
+	if (!flash) {
+		printf("SPI(bus:%u cs:%u) probe failed\n",
+			CONFIG_SF_DEFAULT_BUS,
+			CONFIG_SF_DEFAULT_CS);
+		return 0;
+	}
+
+	/*
+	 * Set the cmd to otp read
+	 */
+	flash->read_cmd = CMD_OTPREAD_ARRAY_FAST;
+	if (spi_flash_read(flash, CONFIG_ZYNQ_GEM_SPI_MAC_OFFSET, 6, ethaddr))
+		printf("SPI MAC address read failed\n");
+
+	if (flash)
+		spi_flash_free(flash);
+
 #endif
 
 	return ret;
